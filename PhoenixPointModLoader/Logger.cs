7136356a41c6b05b33422ad6fea86f7722e72d7e
Sheep-y <your.sheepy@gmail.com>
2020-01-22 13:24:26 +0800

    Initial commit, cloned from PPML and restructured a little bit.

---
+++ PhoenixPointModLoader/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace PhoenixPointModLoader
+{
+    internal static class Logger
+    {
+        internal static string LogPath { get; set; }
+
+        internal static void LogException(string message, Exception e)
+        {
+            using (var logWriter = File.AppendText(LogPath))
+            {
+                logWriter.WriteLine(message);
+                logWriter.WriteLine(e.ToString());
+            }
+        }
+
+        internal static void Log(string message, params object[] formatObjects)
+        {
+            if (string.IsNullOrEmpty(LogPath)) return;
+            using (var logWriter = File.AppendText(LogPath))
+            {
+                logWriter.WriteLine(message, formatObjects);
+            }
+        }
+
+        internal static void LogWithDate(string message, params object[] formatObjects)
+        {
+            if (string.IsNullOrEmpty(LogPath)) return;
+            using (var logWriter = File.AppendText(LogPath))
+            {
+                logWriter.WriteLine(DateTime.Now.ToLongTimeString() + " - " + message, formatObjects);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Modnix Point
+
+A tool to load [Harmony](https://github.com/pardeike/Harmony)-based mods to Snapshot Games' Phoenix Point.
+
+Modnix Point is based on RealityMachina's [Phoenix Point Mod Injector](https://github.com/RealityMachina/PhoenixPointModInjector/),
+combined with experience learned from [ModTek](https://github.com/BattletechModders/ModTek).
+
+This is a work in progress, valid only for further development.
+
+# License
+
+PPML is released to the public domain, so Modnix Point is the same.
