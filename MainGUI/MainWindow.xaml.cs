47ffabd35409475667817da95ce12f721b179421
Sheep-y <your.sheepy@gmail.com>
2020-02-01 19:17:05 +0800

    Implement web button handlers. Name richboxes and fix canny button layout.

--- MainGUI/MainWindow.xaml.cs
+++ MainGUI/MainWindow.xaml.cs
@@ -14,12 +14,34 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MainGUI {
-   /// <summary>
-   /// Interaction logic for MainWindow.xaml
-   /// </summary>
+
    public partial class MainWindow : Window {
       public MainWindow () {
          InitializeComponent();
       }
+
+      private void OpenUrl ( string type, RoutedEventArgs e = null ) {
+         if ( e != null )
+            if ( e.Source is UIElement src && ! src.IsFocused )
+               src.Focus();
+         string url;
+         switch ( type ) {
+            case "canny"  : url = "https://phoenixpoint.canny.io/feedback?sort=trending"; break;
+            case "discord": url = "https://discordapp.com/invite/phoenixpoint"; break;
+            case "forum"  : url = "https://forums.snapshotgames.com/c/phoenix-point"; break;
+            case "manual" : url = "https://drive.google.com/open?id=1n8ORQeDtBkWcnn5Es4LcWBxif7NsXqet"; break;
+            case "reddit" : url = "https://www.reddit.com/r/PhoenixPoint/"; break;
+            case "www"    : url = "https://phoenixpoint.info/"; break;
+            default       : return;
+         }
+         System.Diagnostics.Process.Start( url );
+      }
+
+      private void ButtonCanny_Click   ( object sender, RoutedEventArgs e ) => OpenUrl( "canny", e );
+      private void ButtonDiscord_Click ( object sender, RoutedEventArgs e ) => OpenUrl( "discord", e );
+      private void ButtonForum_Click   ( object sender, RoutedEventArgs e ) => OpenUrl( "forum", e );
+      private void ButtonManual_Click  ( object sender, RoutedEventArgs e ) => OpenUrl( "manual", e );
+      private void ButtonReddit_Click  ( object sender, RoutedEventArgs e ) => OpenUrl( "reddit", e );
+      private void ButtonWebsite_Click ( object sender, RoutedEventArgs e ) => OpenUrl( "www", e );
    }
 }
