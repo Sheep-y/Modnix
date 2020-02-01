49ec2f3a716a38b176d237896d1197738b460e58
Sheep-y <your.sheepy@gmail.com>
2020-02-01 21:58:37 +0800

    Detect modnix version and inject status.

---
+++ MainGUI/AppControl.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sheepy.Modnix.MainGUI {
+   public class AppControl {
+
+      private static string DLL_PATH = "PhoenixPointWin64_Data/Managed";
+      private static string INJECTOR = "ModnixInjector.exe";
+
+      private readonly MainWindow GUI;
+      private readonly object SynRoot = new object();
+
+      private bool Checking;
+
+      public AppControl ( MainWindow _GUI ) => GUI = _GUI;
+
+      public void CheckAppStateAsync () {
+         Task.Run( (Action) CheckAppState );
+      }
+
+      private void CheckAppState () {
+         lock ( SynRoot ) {
+            if ( Checking ) return;
+            Checking = true;
+         }
+         Log( "Checking states" );
+         try {
+            GUI.SetAppVer( Assembly.GetExecutingAssembly().GetName().Version.ToString() );
+            GUI.SetAppState( CheckInjected() );
+
+         } finally {
+            lock ( SynRoot ) {
+               Checking = false;
+            }
+         }
+      }
+
+      public string CheckInjected () {
+         string injector = Path.Combine( DLL_PATH, INJECTOR );
+         try {
+            if ( ! File.Exists( injector ) ) return "missing";
+
+            Log( "Injector found. Detecting injection." );
+            // Start the child process.
+            Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = injector;
+            p.StartInfo.Arguments = "/d";
+            p.StartInfo.WorkingDirectory = DLL_PATH;
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            if ( output == "ppml" || output == "modnix" )
+               return output;
+            return "none";
+         } catch ( Exception ex ) {
+            Log( ex );
+            return "none";
+         }
+      }
+
+      private void Log ( object message ) => GUI.Log( message.ToString() );
+   }
+
+   public enum AppState {
+      UNKNOWN = 0, // Should not happen
+
+      CHECKING = 1,    // App state is being checked
+      NEED_SETUP = 2,  // Not setup
+      INJECTED = 4,    // Already injected
+      NEED_UPDATE = 6, // Already injected, but needs update
+      HAS_UPDATE = 8,  // Has update available on GitHub
+
+      ERROR = 65536,
+      ERR_MISSING = ERROR + 1, // Some files are missing
+   } 
+}
--- MainGUI/MainWindow.xaml.cs
+++ MainGUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,13 +14,58 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Sheepy.Modenix.MainGUI {
+namespace Sheepy.Modnix.MainGUI {
 
    public partial class MainWindow : Window {
 
+      private string AppVer, AppState, GameVer, GameState;
+
       public MainWindow () {
          InitializeComponent();
+         RefreshGUI();
       }
+
+      private void RefreshGUI () {
+         Log( "Initiating Controller" );
+         new AppControl( this ).CheckAppStateAsync();
+      }
+
+      public void Log ( string message ) { Dispatch( () => {
+         textLog.AppendText( message );
+         textLog.AppendText( "\n" );
+      } ); }
+
+      public void SetAppVer ( string value ) { Dispatch( () => {
+         Log( "Modnix version: " + value );
+         AppVer = value;
+         RefreshAppVer();
+      } ); }
+
+      public void SetAppState ( string value ) { Dispatch( () => {
+         Log( "Injection status: " + value );
+         AppState = value;
+         RefreshAppVer();
+      } ); }
+
+      private void RefreshAppVer () {
+         string txt = "Modnix\rVer " + AppVer + "\rStatus: ";
+         if ( AppState == null )
+            txt += "Checking";
+         else
+            switch ( AppState ) {
+               case "ppml" : txt += "PPML found, need update"; break;
+               case "modnix" : txt += "Injected"; break;
+               default: txt += "Need Setup"; break;
+            }
+         richAppInfo.TextRange().Text = txt;
+      }
+
+      private void ButtonCanny_Click   ( object sender, RoutedEventArgs e ) => OpenUrl( "canny", e );
+      private void ButtonDiscord_Click ( object sender, RoutedEventArgs e ) => OpenUrl( "discord", e );
+      private void ButtonForum_Click   ( object sender, RoutedEventArgs e ) => OpenUrl( "forum", e );
+      private void ButtonManual_Click  ( object sender, RoutedEventArgs e ) => OpenUrl( "manual", e );
+      private void ButtonReddit_Click  ( object sender, RoutedEventArgs e ) => OpenUrl( "reddit", e );
+      private void ButtonWebsite_Click ( object sender, RoutedEventArgs e ) => OpenUrl( "www", e );
 
       private void OpenUrl ( string type, RoutedEventArgs e = null ) {
          if ( e != null )
@@ -38,11 +84,12 @@ namespace Sheepy.Modenix.MainGUI {
          System.Diagnostics.Process.Start( url );
       }
 
-      private void ButtonCanny_Click   ( object sender, RoutedEventArgs e ) => OpenUrl( "canny", e );
-      private void ButtonDiscord_Click ( object sender, RoutedEventArgs e ) => OpenUrl( "discord", e );
-      private void ButtonForum_Click   ( object sender, RoutedEventArgs e ) => OpenUrl( "forum", e );
-      private void ButtonManual_Click  ( object sender, RoutedEventArgs e ) => OpenUrl( "manual", e );
-      private void ButtonReddit_Click  ( object sender, RoutedEventArgs e ) => OpenUrl( "reddit", e );
-      private void ButtonWebsite_Click ( object sender, RoutedEventArgs e ) => OpenUrl( "www", e );
+      private void Dispatch ( Action task ) => Dispatcher.Invoke( task );
+   }
+
+   public static class WpfHelper {
+      public static TextRange TextRange ( this RichTextBox box ) {
+         return new TextRange( box.Document.ContentStart, box.Document.ContentEnd );
+      }
    }
 }
