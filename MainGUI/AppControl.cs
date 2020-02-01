e3bcfcf2d0690796280edb7320adf5dd3995f4b4
Sheep-y <your.sheepy@gmail.com>
2020-02-01 23:44:18 +0800

    App code refactor.

--- MainGUI/AppControl.cs
+++ MainGUI/AppControl.cs
@@ -12,6 +12,7 @@ namespace Sheepy.Modnix.MainGUI {
 
       private static string DLL_PATH = "PhoenixPointWin64_Data/Managed";
       private static string INJECTOR = "ModnixInjector.exe";
+      private static string LOADER   = "ModnixLoader.dll";
 
       private readonly MainWindow GUI;
       private readonly object SynRoot = new object();
@@ -24,6 +25,10 @@ namespace Sheepy.Modnix.MainGUI {
          Task.Run( (Action) CheckAppState );
       }
 
+      /// 1. If injector is in correct place = call injector to detect status
+      /// 2. If injector is not in place, but dummy exists
+      ///   Check Phoenix Point. Found = can setup. Not found = Error.
+      /// 3. If dummy not exists = Error, re-download
       private void CheckAppState () {
          lock ( SynRoot ) {
             if ( Checking ) return;
@@ -32,7 +37,12 @@ namespace Sheepy.Modnix.MainGUI {
          Log( "Checking states" );
          try {
             GUI.SetAppVer( Assembly.GetExecutingAssembly().GetName().Version.ToString() );
-            GUI.SetAppState( CheckInjected() );
+            GUI.SetAppState( CheckInjected( out bool hasInjector ) );
+            if ( hasInjector ) {
+
+            } else {
+
+            }
 
          } finally {
             lock ( SynRoot ) {
@@ -41,45 +51,54 @@ namespace Sheepy.Modnix.MainGUI {
          }
       }
 
-      public string CheckInjected () {
-         string injector = Path.Combine( DLL_PATH, INJECTOR );
-         try {
-            if ( ! File.Exists( injector ) ) return "missing";
+      public readonly string InjectorPath = Path.Combine( DLL_PATH, INJECTOR );
+      public readonly string LoaderPath   = Path.Combine( DLL_PATH, LOADER   );
 
-            Log( "Injector found. Detecting injection." );
-            // Start the child process.
-            Process p = new Process();
-            // Redirect the output stream of the child process.
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.FileName = injector;
-            p.StartInfo.Arguments = "/d";
-            p.StartInfo.WorkingDirectory = DLL_PATH;
-            p.Start();
-            string output = p.StandardOutput.ReadToEnd();
-            p.WaitForExit();
-            if ( output == "ppml" || output == "modnix" )
-               return output;
-            return "none";
+      public bool InjectorInPlace () { try {
+         if ( ! File.Exists( InjectorPath ) ) return Log( $"Injector not found: {InjectorPath}", false );
+         if ( ! File.Exists( LoaderPath ) ) return Log( $"Loader not found: {LoaderPath}", false );
+         return Log( $"Injector and loader found in {DLL_PATH}", true );
+      } catch ( IOException ex ) {
+         return Log( ex, false );
+      } }
+
+      public string CheckInjected ( out bool hasInjector ) {
+         hasInjector = false;
+         try {
+            if ( ! InjectorInPlace() ) return "missing";
+            Log( "Detecting injection status." );
+            string state = RunAndWait( DLL_PATH, InjectorPath, "/d" ).Trim();
+            if ( state == "ppml" || state == "modnix" ) {
+               hasInjector = true;
+               return state;
+            }
+            return Log( $"Unknown result: {state}", "none" );
          } catch ( Exception ex ) {
-            Log( ex );
-            return "none";
+            return Log( ex, "error" );
          }
       }
 
+      private string RunAndWait ( string path, string exe, string param = null ) {
+         Log( $"Running at {path} : {exe} {param}" );
+         
+         Process p = new Process();
+         p.StartInfo.UseShellExecute = false;
+         p.StartInfo.RedirectStandardOutput = true;
+         p.StartInfo.FileName = exe;
+         p.StartInfo.Arguments = param;
+         p.StartInfo.WorkingDirectory = path;
+         p.Start();
+
+         string output = p.StandardOutput.ReadToEnd();
+         p.WaitForExit();
+         return output;
+      }
+
       private void Log ( object message ) => GUI.Log( message.ToString() );
+
+      private T Log<T> ( object message, T result ) {
+         Log( message.ToString() );
+         return result;
+      }
    }
-
-   public enum AppState {
-      UNKNOWN = 0, // Should not happen
-
-      CHECKING = 1,    // App state is being checked
-      NEED_SETUP = 2,  // Not setup
-      INJECTED = 4,    // Already injected
-      NEED_UPDATE = 6, // Already injected, but needs update
-      HAS_UPDATE = 8,  // Has update available on GitHub
-
-      ERROR = 65536,
-      ERR_MISSING = ERROR + 1, // Some files are missing
-   } 
 }
