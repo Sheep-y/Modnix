733943ea373f1081f864538255690bf2462d3cbd
Sheep-y <your.sheepy@gmail.com>
2020-02-11 21:21:06 +0800

    New class to share prompt message between windows.

--- MainGUI/MainWindow.xaml.cs
+++ MainGUI/MainWindow.xaml.cs
@@ -126,31 +126,10 @@ namespace Sheepy.Modnix.MainGUI {
 
       public void Prompt ( string parts, Exception ex = null ) { Dispatch( () => {
          Log( $"Prompt {parts}" );
-         string txt = "";
-         if ( parts.StartsWith( "setup_ok" ) ) {
-            txt = $"Setup success.\nCheck status after every game patch.\n\nMod folder:\nMy Documents\\{AppControl.MOD_PATH}\n";
-            if ( parts.Contains( ",mod_moved" ) )
-               txt += "\nMods moved to new mod folder.";
-            if ( parts.Contains( ",self_copy" ) )
-               txt += "\nModnix installed to mod folder.";
-            if ( parts.Contains( ",ppml" ) )
-               txt += "\nPPML renamed to prevent accidents.";
-            if ( parts.Contains( ",self_copy" ) ) {
-               txt += "\n\nThis setup file may be deleted.\nStarting Modnix now.";
-               if ( MessageBox.Show( txt, "Success", MessageBoxButton.OKCancel, MessageBoxImage.Information ) == MessageBoxResult.OK ) {
-                  Process.Start( App.ModGuiExe, "/i " + Process.GetCurrentProcess().Id );
-                  Close();
-               }
-            } else {
-               MessageBox.Show( txt, "Success", MessageBoxButton.OK, MessageBoxImage.Information );
-            }
-         } else if ( parts.StartsWith( "restore_ok" ) ) {
-            MessageBox.Show( "Uninstall successful.", "Success" );
-         } else if ( parts.StartsWith( "error" ) ) {
-            txt = "Action failed. See log for details.";
-            if ( ex != null ) txt += "Error: " + ex;
-            MessageBox.Show(  txt , "Error", MessageBoxButton.OK, MessageBoxImage.Error );
-         }
+         SharedGui.Prompt( parts, ex, () => {
+            Process.Start( App.ModGuiExe, "/i " + Process.GetCurrentProcess().Id );
+            Close();
+         } );
       } ); }
 
       private void ButtonNexus_Click ( object sender, RoutedEventArgs e ) => OpenUrl( "nexus", e );
---
+++ MainGUI/SharedGui.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sheepy.Modnix.MainGUI {
+   internal class SharedGui {
+      internal static void Prompt ( string parts, Exception ex, Action OnRestart ) {
+         string txt;
+         if ( parts.StartsWith( "setup_ok" ) ) {
+            txt = $"Setup success.\nCheck status after every game patch.\n\nMod folder:\nMy Documents\\{AppControl.MOD_PATH}\n";
+            if ( parts.Contains( ",mod_moved" ) )
+               txt += "\nMods moved to new mod folder.";
+            if ( parts.Contains( ",self_copy" ) )
+               txt += "\nModnix installed to mod folder.";
+            if ( parts.Contains( ",ppml" ) )
+               txt += "\nPPML renamed to prevent accidents.";
+            if ( parts.Contains( ",self_copy" ) ) {
+               txt += "\n\nThis setup file may be deleted.\nStarting Modnix now.";
+               if ( MessageBox.Show( txt, "Success", MessageBoxButton.OKCancel, MessageBoxImage.Information ) == MessageBoxResult.OK )
+                  OnRestart();
+            } else {
+               MessageBox.Show( txt, "Success", MessageBoxButton.OK, MessageBoxImage.Information );
+            }
+         } else if ( parts.StartsWith( "restore_ok" ) ) {
+            MessageBox.Show( "Uninstall successful.", "Success" );
+         } else if ( parts.StartsWith( "error" ) ) {
+            txt = "Action failed. See log for details.";
+            if ( ex != null ) txt += "Error: " + ex;
+            MessageBox.Show(  txt , "Error", MessageBoxButton.OK, MessageBoxImage.Error );
+         }
+      }
+   }
+}
