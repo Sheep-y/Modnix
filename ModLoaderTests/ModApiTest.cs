a0a77415c0481436988553fe489d7aefdd62c821
Sheep-y <your.sheepy@gmail.com>
2020-03-22 12:41:18 +0800

    Test all informative apis.

--- ModLoaderTests/ModApiTest.cs
+++ ModLoaderTests/ModApiTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using static System.Reflection.BindingFlags;
 
 namespace Sheepy.Modnix.Tests {
@@ -9,7 +11,7 @@ namespace Sheepy.Modnix.Tests {
 
       private static ModEntry ModA = new ModEntry( "//A", new ModMeta () { Id = "Test.A", Version = new Version( 1, 0, 0, 0 ) } );
       private static ModEntry ModB = new ModEntry( "//B/b", new ModMeta () { Id = "Test.B", Version = new Version( 1, 2, 3, 4 ) } );
-      private static ModEntry ModC = new ModEntry( "C:\\C", new ModMeta () { Id = "Test.C", Version = null } );
+      private static ModEntry ModC = new ModEntry( new ModMeta () { Id = "Test.C", Version = null } );
       private static Version ZeroVersion = new Version( 0, 0, 0, 0 );
 
       [ClassInitializeAttribute] public static void TestInitialize ( TestContext _ ) {
@@ -35,7 +37,48 @@ namespace Sheepy.Modnix.Tests {
          Assert.AreEqual( ModLoader.GameVersion, ModA.ModAPI( "version", "game" ), "game" );
 
          Assert.AreEqual( ModB.Metadata.Version, ModA.ModAPI( "version", "Test.B" ), "Test.B" );
-         Assert.AreEqual( ZeroVersion, ModA.ModAPI( "version", "test.c" ), "test." );
+         Assert.AreEqual( ZeroVersion, ModA.ModAPI( "version", "test.c" ), "test.c" );
+      }
+
+      [TestMethod()] public void PathTest () {
+         Assert.AreEqual( ModA.Path, ModA.ModAPI( "path", null ), "A null" );
+         Assert.AreEqual( ModA.Path, ModA.ModAPI( "path", "" ), "A empty" );
+         Assert.AreEqual( ModA.Path, ModA.ModAPI( "path", " " ), "A blank" );
+
+         Assert.AreEqual( ModLoader.ModDirectory, ModA.ModAPI( "path", "mods_root" ), "root" );
+
+         Assert.AreEqual( ModB.Path, ModA.ModAPI( "path", "Test.B" ), "Test.B" );
+         Assert.AreEqual( null, ModA.ModAPI( "path", "test.c" ), "test.c" );
+      }
+
+      [TestMethod()] public void ModTest () {
+         Assert.AreEqual( ModA, ModA.ModAPI( "mod", null ), "A null" );
+         Assert.AreEqual( ModA, ModA.ModAPI( "mod", "" ), "A empty" );
+         Assert.AreEqual( ModA, ModA.ModAPI( "mod", " " ), "A blank" );
+         Assert.AreEqual( ModB, ModA.ModAPI( "mod", "Test.B" ), "Test.B" );
+         Assert.AreEqual( ModC, ModA.ModAPI( "mod", "test.c" ), "test.c" );
+      }
+
+      [TestMethod()] public void ModInfoTest () {
+         Assert.AreEqual( ModA.Metadata, ModA.ModAPI( "mod_info", null ), "A null" );
+         Assert.AreEqual( ModA.Metadata, ModA.ModAPI( "mod_info", "" ), "A empty" );
+         Assert.AreEqual( ModA.Metadata, ModA.ModAPI( "mod_info", " " ), "A blank" );
+         Assert.AreEqual( ModB.Metadata, ModA.ModAPI( "mod_info", "Test.B" ), "Test.B" );
+         Assert.AreEqual( ModC.Metadata, ModA.ModAPI( "mod_info", "test.c" ), "test.c" );
+      }
+
+      [TestMethod()] public void ModListTest () {
+         var list = (IEnumerable<string>) ModA.ModAPI( "mod_list", null );
+         Assert.AreEqual( 3, list.Count(), "list count" );
+         Assert.IsTrue( list.Contains( ModA.Metadata.Id ), "A" );
+         Assert.IsTrue( list.Contains( ModB.Metadata.Id ), "B" );
+         Assert.IsTrue( list.Contains( ModC.Metadata.Id ), "C" );
+
+         list = (IEnumerable<string>) ModA.ModAPI( "mod_list", "test" );
+         Assert.AreEqual( 3, list.Count(), "test list count" );
+
+         list = (IEnumerable<string>) ModA.ModAPI( "mod_list", "none" );
+         Assert.AreEqual( 0, list.Count(), "empty list count" );
       }
    }
 }
