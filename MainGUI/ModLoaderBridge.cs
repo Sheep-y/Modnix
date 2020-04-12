Sheep-y/Modnix

commit b1408687c2431d533155282c9781f878b147c8e8

Remove "Can create config file" notice since it is managed by the config tab now.

MainGUI/ModLoaderBridge.cs
@@ -454,10 +454,6 @@ namespace Sheepy.Modnix.MainGUI {
                if ( path == self ) selfRun.Text = txt;
                else list.Add( txt );
             }
-         if ( Mod.HasConfig ) {
-            var config = Mod.CheckConfigFile();
-            list.Add( config != null ? $"\r{fileName(config)} [Config]" : "\r(Can create config file)" );
-         }
          if ( Docs != null ) {
             foreach ( var row in Docs )
                if ( row.Value != "embedded" )

commit 0142e204ab85484f0d3693f648dc7577fbccbcba

Show config file in file list. This is a regression from b1408687c2431d533155282c9781f878b147c8e8.

MainGUI/ModLoaderBridge.cs
@@ -454,6 +454,10 @@ namespace Sheepy.Modnix.MainGUI {
                if ( path == self ) selfRun.Text = txt;
                else list.Add( txt );
             }
+         if ( Mod.HasConfig ) {
+            var config = Mod.CheckConfigFile();
+            if ( config != null ) list.Add( $"\r{fileName(config)} [Config]" );
+         }
          if ( Docs != null ) {
             foreach ( var row in Docs )
                if ( row.Value != "embedded" )