Sheep-y/Modnix

commit c30604ccf4021ae35a11da3912be6889e9287ad5

Rewrite all file read to ignore file locks.

MainGUI/App.xaml.cs
@@ -320,7 +320,7 @@ namespace Sheepy.Modnix.MainGUI {
             if ( File.Exists( logFile ) &&
                  File.GetLastWriteTime( logFile ) > File.GetLastWriteTime( Path.Combine( CurrentGame.CodeDir, GAME_DLL ) ) ) {
                Log( $"Parsing {logFile}" );
-               var line = File.ReadLines( logFile ).ElementAtOrDefault( 1 );
+               var line = Utils.ReadLine( logFile );
                var match = Regex.Match( line ?? "", "Assembly-CSharp/([^ ;]+)", RegexOptions.IgnoreCase );
                if ( match.Success )
                   return match.Value.Substring( 16 );
@@ -890,11 +890,11 @@ namespace Sheepy.Modnix.MainGUI {
    }
 
    internal static class Utils {
-      internal static string ReadFile ( string file ) {
-         using ( var reader = new StreamReader( new FileStream( file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete ) ) ) {
-            return reader.ReadToEnd();
-         }
-      }
+      private static StreamReader Read ( string file ) =>
+         new StreamReader( new FileStream( file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete ), Encoding.UTF8, true );
+
+      internal static string ReadFile ( string file ) { using ( var reader = Read( file ) ) return reader.ReadToEnd(); }
+      internal static string ReadLine ( string file ) { using ( var reader = Read( file ) ) return reader.ReadLine(); }
    }
 
    internal static class ExtCls {

MainGUI/MainWindow.xaml.cs
@@ -783,10 +783,7 @@ namespace Sheepy.Modnix.MainGUI {
             try {
                TextLicense.Text = ApplyLogFilter( Utils.ReadFile( isLoader ? App.LoaderLog : App.ConsoleLog ) );
             } catch ( SystemException ex ) {
-               if ( isConsole && SharedGui.IsGameRunning )
-                  TextLicense.Text = "The game locks the console log.  Cannot read it when game is running.";
-               else
-                  TextLicense.Text = ex.ToString();
+               TextLicense.Text = ex.ToString();
             }
             ConsoleLogTime = new FileInfo( App.ConsoleLog ).LastWriteTime;
             LoaderLogTime  = new FileInfo( App.LoaderLog  ).LastWriteTime;

MainGUI/ModLoaderBridge.cs
@@ -337,7 +337,7 @@ namespace Sheepy.Modnix.MainGUI {
          string text = null;
          if ( Docs.TryGetValue( type, out string file ) && ! "embedded".Equals( file, StringComparison.Ordinal ) ) {
             Log( $"Reading {type} {file}" );
-            text = File.ReadAllText( file );
+            text = Utils.ReadFile( file );
          } else {
             Log( $"Reading embedded {type} from {Path}" );
             var buf = new StringBuilder();

ModLoader/ModEntry.cs
@@ -454,7 +454,7 @@ namespace Sheepy.Modnix {
             if ( meta.ConfigText != null )
                return meta.ConfigText;
          if ( confFile == null ) confFile = CheckConfigFile();
-         return meta.ConfigText = confFile != null ? File.ReadAllText( confFile, Encoding.UTF8 ) : GetDefaultConfigText();
+         return meta.ConfigText = confFile != null ? Tools.ReadFile( confFile ) : GetDefaultConfigText();
       } catch ( Exception ex ) { Error( ex ); return null; } }
 
       public string CacheDefaultConfigText ( string config ) {

ModLoader/ModLoader.cs
@@ -140,7 +140,7 @@ namespace Sheepy.Modnix {
          var confFile = Path.Combine( ModDirectory, CONF_FILE );
          if ( File.Exists( confFile ) ) try {
             Log.Info( $"Loading {confFile}" );
-            Settings = ModMetaJson.Parse<LoaderSettings>( File.ReadAllText( confFile ) );
+            Settings = ModMetaJson.Parse<LoaderSettings>( Tools.ReadFile( confFile ) );
          } catch ( Exception ex ) { Log.Error( ex ); }
          if ( Settings == null ) {
             Log.Info( $"Using default settings, because cannot find or parse {confFile}" );
@@ -278,5 +278,11 @@ namespace Sheepy.Modnix {
 
    internal static class Tools {
       internal static string FixSlash ( this string path ) => path.Replace( '/', Path.DirectorySeparatorChar );
+   
+      private static StreamReader Read ( string file ) =>
+         new StreamReader( new FileStream( file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete ), Encoding.UTF8, true );
+
+      internal static string ReadFile ( string file ) { using ( var reader = Read( file ) ) return reader.ReadToEnd(); }
+      internal static string ReadLine ( string file ) { using ( var reader = Read( file ) ) return reader.ReadLine(); }
    }
 }

ModLoader/ModScanner.cs
@@ -104,7 +104,7 @@ namespace Sheepy.Modnix {
             var default_id = Path.GetFileNameWithoutExtension( file );
             if ( string.IsNullOrWhiteSpace( default_id ) || default_id.Equals( "mod_info", StringComparison.OrdinalIgnoreCase ) )
                default_id = container;
-            meta = ParseInfoJs( File.ReadAllText( file, Encoding.UTF8 ).Trim(), default_id );
+            meta = ParseInfoJs( Tools.ReadFile( file ).Trim(), default_id );
             if ( meta == null ) return null;
             if ( ! meta.HasContent )
                meta.Dlls = Directory.EnumerateFiles( Path.GetDirectoryName( file ), "*.dll" )

commit 2c266686d3061b0f8de7dc87bf06f707a580d41d

Fix game version not parsed from game log. Regression from commit c30604ccf4021ae35a11da3912be6889e9287ad5.

MainGUI/App.xaml.cs
@@ -320,7 +320,7 @@ namespace Sheepy.Modnix.MainGUI {
             if ( File.Exists( logFile ) &&
                  File.GetLastWriteTime( logFile ) > File.GetLastWriteTime( Path.Combine( CurrentGame.CodeDir, GAME_DLL ) ) ) {
                Log( $"Parsing {logFile}" );
-               var line = Utils.ReadLine( logFile );
+               var line = Utils.ReadLine( logFile, 1 );
                var match = Regex.Match( line ?? "", "Assembly-CSharp/([^ ;]+)", RegexOptions.IgnoreCase );
                if ( match.Success )
                   return match.Value.Substring( 16 );
@@ -900,7 +900,12 @@ namespace Sheepy.Modnix.MainGUI {
          new StreamReader( new FileStream( file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete ), Encoding.UTF8, true );
 
       internal static string ReadFile ( string file ) { using ( var reader = Read( file ) ) return reader.ReadToEnd(); }
-      internal static string ReadLine ( string file ) { using ( var reader = Read( file ) ) return reader.ReadLine(); }
+      internal static string ReadLine ( string file, int skipLine = 0 ) {
+         using ( var reader = Read( file ) ) {
+            while ( skipLine-- > 0 ) reader.ReadLine();
+            return reader.ReadLine();
+         }
+      }
    }
 
    internal static class ExtCls {