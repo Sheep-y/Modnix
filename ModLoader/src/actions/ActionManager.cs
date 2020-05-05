2adbbea91349cc24aac756e1d723b3c46174e4d5
Sheep-y <your.sheepy@gmail.com>
2020-05-05 13:33:33 +0800

    Prototype eval code... that does not work.
    System.SystemException: Error running C:\Program Files\mono\mini\mono.exe: The system cannot find the file specified.  at Microsoft.CSharp.CSharpCodeGenerator.FromFileBatch

---
+++ ModLoader/src/actions/ActionManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sheepy.Modnix.Actions {
+
+   internal class ActionManager {
+
+      internal static void RunAction ( ModEntry mod, string phase ) {
+         ModAction[] actions;
+         lock ( mod.Metadata ) actions = mod.Metadata.Actions;
+         if ( actions == null ) return;
+         mod.CreateLogger().Info( "Running {0} actions" );
+
+         List<ModAction> evals = null;
+         foreach ( var a in actions ) try {
+            if ( Array.IndexOf( a.Phase, phase ) < 0 ) continue;
+            if ( a.Eval != null ) {
+               if ( evals == null ) evals = new List<ModAction>();
+               evals.Add( a );
+            } else
+               RunAction( a );
+         } catch ( Exception ex ) { mod.CreateLogger().Error( ex ); }
+
+         if ( evals != null ) try {
+            EvalAction.Run( mod, evals.ToArray() );
+         } catch ( Exception ex ) { mod.CreateLogger().Error( ex ); }
+      }
+
+      internal static void RunAction ( ModAction action ) {
+         throw new NotImplementedException();
+      }
+   }
+
+}
--- ModLoader/src/actions/EvalAction.cs
+++ ModLoader/src/actions/EvalAction.cs
@@ -1,4 +1,5 @@
-﻿using Sheepy.Logging;
+﻿using Microsoft.CSharp;
+using Sheepy.Logging;
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
@@ -11,15 +12,18 @@ using System.Threading.Tasks;
 namespace Sheepy.Modnix.Actions {
    internal class EvalAction {
 
-      private readonly ModEntry mod;
+      private readonly ModEntry Mod;
 
-      private Logger Log => mod.CreateLogger();
+      private Logger Log => Mod.CreateLogger();
 
-      public EvalAction ( ModEntry mod ) => this.mod = mod;
+      public EvalAction ( ModEntry mod ) => this.Mod = mod;
 
-      internal static void Run ( ModEntry mod, ModAction[] actions ) => new EvalAction( mod ).Run( actions );
+      internal static void Run ( ModEntry mod, ModAction[] actions ) {
+         if ( actions == null || actions.Length == 0 ) return;
+         new EvalAction( mod ).Run( actions );
+      }
 
-      private static readonly string[] Assemblies = new string[]{ "0Harmony.dll", "Newtonsoft.Json.dll", };
+      private static readonly string[] Assemblies = new string[]{ "0Harmony.dll", "Newtonsoft.Json.dll" };
       private static StringBuilder Usings;
       private static CompilerParameters Params;
 
@@ -45,7 +49,7 @@ namespace Sheepy.Modnix.Actions {
             names.Clear();
 
             int capacity = 0;
-            foreach ( var asm in AppDomain.CurrentDomain.GetAssemblies() ) {
+            foreach ( var asm in AppDomain.CurrentDomain.GetAssemblies() ) try {
                Type[] types;
                if ( asm.IsDynamic || ! refs.Contains( asm.Location ) ) continue;
                ModLoader.Log.Verbo( asm.Location );
@@ -57,11 +61,10 @@ namespace Sheepy.Modnix.Actions {
                foreach ( var t in types ) {
                   if ( t?.IsPublic != true ) continue;
                   names.Add( t.Namespace );
-                  capacity += 7 + t.Namespace.Length;
                }
-            }
+            } catch ( Exception ex ) { ModLoader.Log.Warn( ex ); }
 
-            Usings = new StringBuilder( capacity );
+            Usings = new StringBuilder( names.Sum( e => e.Length + 7 ) );
             foreach ( var name in names )
                Usings.Append( "using " ).Append( name ).Append( ';' );
             ModLoader.Log.Verbo( Usings );
@@ -69,8 +72,41 @@ namespace Sheepy.Modnix.Actions {
       }
 
       private void Run ( ModAction[] actions ) {
+         var mainFunc = Compile( BuildActionCode( actions ) );
+         if ( mainFunc == null ) return;
+         Log.Verbo( "Calling compiled actions", actions );
+         mainFunc.Invoke( null, new object[]{ Mod } );
+      }
+
+      private string BuildActionCode ( ModAction[] actions ) {
          PrepareReferences();
-         Log.Verbo( "Compiling {0} eval actions", actions.Length );
+         Log.Verbo( "Buliding {0} eval actions", actions.Length );
+         StringBuilder code = new StringBuilder( 2048 ), main = new StringBuilder( 256 );
+         code.Append( "static class " ).Append( Mod.Key ).Append( "{" );
+         code.Append( "static Sheepy.Modnix.ModEntry Mod;" );
+         code.Append( "static object Api(string a,object p=null)=>Mod.ModAPI(a,p);" );
+         int id = 0;
+         foreach ( var act in actions ) {
+            if ( act?.Eval == null ) continue; // TODO: Add random brackets to prevent jailbreak
+            code.Append( "static void Action" ).Append( id ).Append( "(){" ).Append( act.Eval ).Append( ";}" );
+            main.Append( "Action" ).Append( id++ ).Append( "();" );
+         }
+         code.Append( "public static void Eval(Sheepy.Modnix.ModEntry mod){" );
+         code.Append( "Api(\"log\",\"Evaluating\");" );
+         code.Append( main ).Append( "}}" );
+         return new StringBuilder( Usings.Length + code.Length ).Append( Usings ).Append( code ).ToString();
+      }
+
+      private MethodInfo Compile ( string code ) {
+         Log.Verbo( "Compiling {0} chars", code.Length );
+         var result = new CSharpCodeProvider().CompileAssemblyFromSource( Params, code );
+         if ( result.Errors.Count > 0 ) {
+            Log.Error( "Cannot compile {0}", code );
+            foreach ( var line in result.Errors )
+               Log.Error( line );
+            return null;
+         }
+         return result.CompiledAssembly.GetType( "global::" + Mod.Key ).GetMethod( "Eval" );
       }
    }
 }
