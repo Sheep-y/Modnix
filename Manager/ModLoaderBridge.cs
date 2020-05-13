c0c0074ebcb7fe858fb276811a78ee73a47877c9
Sheep-y <your.sheepy@gmail.com>
2020-05-13 22:32:51 +0800

    Sort notice code.

--- Manager/ModLoaderBridge.cs
+++ Manager/ModLoaderBridge.cs
@@ -390,18 +390,18 @@ namespace Sheepy.Modnix.MainGUI {
       private static Run FormatNotice ( LogEntry notice ) {
          var txt = new Run();
          switch ( notice.Message ) {
-            case "duplicate" :
-               txt.Text = string.Format( "\rDisabled: Duplicate of {0}.", notice.Args[0]?.ToString() ); break;
             case "avoid" :
                txt.Text = string.Format( "\rDisabled: Avoiding {0}.", notice.Args[0]?.ToString() ); break;
-            case "require" :
-               txt.Text = string.Format( "\rDisabled: Missing requirement {0}.", notice.Args[0]?.ToString() ); break;
             case "disable" :
                txt.Text = string.Format( "\rDisabled by {0}.", notice.Args[0]?.ToString() ); break;
+            case "duplicate" :
+               txt.Text = string.Format( "\rDisabled: Duplicate of {0}.", notice.Args[0]?.ToString() ); break;
             case "manual"  :
                txt.Text = "\rManually Disabled"; break;
             case "no_phase"  :
                txt.Text = "\rDisabled: No mod phase found."; break;
+            case "require" :
+               txt.Text = string.Format( "\rDisabled: Missing requirement {0}.", notice.Args[0]?.ToString() ); break;
             case "runtime_error" :
                txt.Text = "\rRuntime error(s) detected on last run, may be not safe to use."; break;
             case "runtime_warning" :
