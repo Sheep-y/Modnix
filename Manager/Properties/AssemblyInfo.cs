09ae71b646e1c7eb8a31f66b141c99cf5f8ef66d
Sheep-y <your.sheepy@gmail.com>
2021-01-11 11:34:28 +0800

    Bumping version for development release.

--- Manager/Properties/AssemblyInfo.cs
+++ Manager/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "3.0.2020.0711" )]
-[assembly: AssemblyFileVersion( "3.0.2020.0711" )]
+[assembly: AssemblyVersion( "3.0.2021.0111" )]
+[assembly: AssemblyFileVersion( "3.0.2021.0111" )]
 [assembly: NeutralResourcesLanguage( "en" ) ]
