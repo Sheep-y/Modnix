664e4a697bf304dc8251ca69524633976a66374c
Sheep-y <your.sheepy@gmail.com>
2021-01-25 14:42:11 +0800

    Fix demo archive structure and bump version.

--- Manager/Properties/AssemblyInfo.cs
+++ Manager/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "3.0.2021.0120" )]
-[assembly: AssemblyFileVersion( "3.0.2021.0120" )]
+[assembly: AssemblyVersion( "3.0.2021.0125" )]
+[assembly: AssemblyFileVersion( "3.0.2021.0125" )]
 [assembly: NeutralResourcesLanguage( "en" ) ]
